19e3dba383a4184500ad594dd0654e95464cdf47
CledsonSilvaAraujo <ciebinho.embetant@gmail.com>
2022-07-06 21:11:38 -0300

    one signal working

--- android/src/br/uff/ic/lek/AndroidLauncher.java
+++ android/src/br/uff/ic/lek/AndroidLauncher.java
@@ -3,6 +3,7 @@ package br.uff.ic.lek;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -21,25 +22,106 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.zip.CRC32;
-import br.uff.ic.lek.Alquimia;
-
-// https://developers.google.com/android/guides/client-auth
-// https://www.geeksforgeeks.org/create-and-add-data-to-firebase-firestore-in-android/
 
 
 public class AndroidLauncher extends AndroidApplication {
 	private static final String TAG = "JOGO";
 	private static final String ONESIGNAL_APP_ID = "92576ca0-c121-46bb-a1bc-24ca96e1173d";
-	private static final String ONESIGNAL_API_KEY = "ODlmNWZhMjUtZmJlYS00MTA5LTg5YTAtYWJkZjA5YTJiNmM1";
-	private static final String ONESIGNAL_USER_KEY_TOKEN = "MjAyYmY1ZDItOGY0Yi00MmQyLWE3YmUtZWFiNmZhNjcyZjc5";
+	private static final String ONESIGNAL_API_KEY = "NmRkYTkyZGEtNDQwYS00OTFkLTlhNjgtMDNmYWQxODlhZmY1";
+	private static final String ONESIGNAL_USER_KEY_TOKEN = "YmE4OWIwNDctYzMyYi00MWFkLWI2YmQtYmRjYTA3MDVhZmFh";
 	protected String playerNickName;
 	protected String emailCRC32;
 	protected String pwdCRC32;
 	protected int runningTimes;
 	protected String sharedPreferencesName = "ALCH0005";
 
+	@Override
+	public void onCreate (Bundle savedInstanceState) {
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+		super.onCreate(savedInstanceState);
 
-	protected void defaultAccountGenerator() {
+		defaultAccountGenerator();
+		Log.d(TAG, "################ playerNickName=<"+playerNickName+">################");
+
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		config.useWakelock = true;
+		config.useAccelerometer = true;
+		useImmersiveMode (true);
+		initialize(
+			new Alquimia(new AndroidInterfaceClass(playerNickName, emailCRC32, pwdCRC32, runningTimes)),
+			config
+		);
+		// Enable verbose OneSignal logging to debug issues if needed.
+		OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+		// OneSignal Initialization
+		OneSignal.initWithContext(this);
+		OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+		// GRUPO-01-VERDE: Set User Firebase ID as External ID in OneSignal.
+		PlayerData player = PlayerData.myPlayerData();
+		OneSignal.setExternalUserId(player.getAuthUID());
+
+		try {
+			invitePlayers();
+		} catch (ApiException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void invitePlayers () throws ApiException {
+		ApiClient defaultClient = Configuration.getDefaultApiClient();
+		defaultClient.setBasePath("https://onesignal.com/api/v1");
+
+		String appKeyToken = ONESIGNAL_API_KEY;
+		String userKeyToken = ONESIGNAL_USER_KEY_TOKEN;
+
+		HttpBearerAuth appKey = (HttpBearerAuth) defaultClient.getAuthentication("app_key");
+		appKey.setBearerToken(appKeyToken);
+		HttpBearerAuth userKey = (HttpBearerAuth) defaultClient.getAuthentication("user_key");
+		userKey.setBearerToken(userKeyToken);
+
+		DefaultApi apiInstance = new DefaultApi(defaultClient);
+
+		Notification notification = new Notification();
+		notification.setAppId(ONESIGNAL_APP_ID);
+		StringMap message = new StringMap();
+		message.setPt("Venha jogar Alquimia!");
+		message.setEn("Come play Alquimia!");
+		notification.setContents(message);
+
+		String appId = ONESIGNAL_APP_ID;
+		OSDeviceState thisDevice = OneSignal.getDeviceState();
+		String thisDeviceId = thisDevice.getUserId();
+		PlayerSlice players = null;
+
+		try {
+			players = apiInstance.getPlayers(appId, 300, 0);
+		} catch (ApiException e) {
+			e.printStackTrace();
+		}
+
+		for (Player player: players.getPlayers()) {
+			if (!player.getId().equals(thisDeviceId))
+				notification.addIncludePlayerIdsItem(player.getId());
+		}
+
+		try {
+			apiInstance.createNotification(notification);
+			System.out.println("OneSignal Invite Sent Successfully!");
+		} catch (ApiException e) {
+			System.err.println("Exception when calling DefaultApi#createNotification");
+			System.err.println("Status code: " + e.getCode());
+			System.err.println("Reason: " + e.getResponseBody());
+			System.err.println("Response headers: " + e.getResponseHeaders());
+			e.printStackTrace();
+		}
+	}
+
+	private void defaultAccountGenerator() {
 		Date date = Calendar.getInstance().getTime();
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		String strDate = dateFormat.format(date);
@@ -97,101 +179,5 @@ public class AndroidLauncher extends AndroidApplication {
 		Log.d(TAG, "################ saved<"+pwdCRC32+">################");
 		Log.d(TAG, "################ saved<"+emailCRC32+">################");
 		Log.d(TAG, "################ run "+runningTimes+" X ################");
-	}
-
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		defaultAccountGenerator();
-		Log.d(TAG, "################ playerNickName=<"+playerNickName+">################");
-
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-
-		config.useWakelock = true;
-		config.useAccelerometer = true;
-		useImmersiveMode (true);
-		initialize(
-			new Alquimia(new AndroidInterfaceClass(playerNickName, emailCRC32, pwdCRC32, runningTimes)),
-			config
-		);
-		// Enable verbose OneSignal logging to debug issues if needed.
-		OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
-
-		// OneSignal Initialization
-		OneSignal.initWithContext(this);
-		OneSignal.setAppId(ONESIGNAL_APP_ID);
-
-		// GRUPO-01-VERDE: Set User Firebase ID as External ID in OneSignal.
-		PlayerData player = PlayerData.myPlayerData();
-		OneSignal.setExternalUserId(player.getAuthUID());
-
-//		try {
-//			invitePlayers();
-//		} catch (ApiException e) {
-//			e.printStackTrace();
-//		}
-
-	}
-
-	private void invitePlayers () throws ApiException {
-		// OneSignal API Client
-		ApiClient defaultClient = Configuration.getDefaultApiClient();
-		defaultClient.setBasePath("https://onesignal.com/api/v1");
-
-		// Configure HTTP bearer authorization: app_key
-		String appKeyToken = ONESIGNAL_API_KEY;
-		String userKeyToken = ONESIGNAL_USER_KEY_TOKEN;
-
-		HttpBearerAuth appKey = (HttpBearerAuth) defaultClient.getAuthentication("app_key");
-		appKey.setBearerToken(appKeyToken);
-		HttpBearerAuth userKey = (HttpBearerAuth) defaultClient.getAuthentication("user_key");
-		userKey.setBearerToken(userKeyToken);
-
-		// Api Instance
-		DefaultApi apiInstance = new DefaultApi(defaultClient);
-
-		// Create OneSignal Notification Object
-		Notification notification = new Notification();
-		notification.setAppId(ONESIGNAL_APP_ID);
-		StringMap message = new StringMap();
-		message.setPt("Venha jogar Alquimia!");
-		message.setEn("Come play Alquimia!");
-		notification.setContents(message);
-
-		// Get Players
-		String appId = ONESIGNAL_APP_ID;
-		OSDeviceState thisDevice = OneSignal.getDeviceState();
-		String thisDeviceId = thisDevice.getUserId();
-		PlayerSlice players = null;
-
-		try {
-			players = apiInstance.getPlayers(appId, 300, 0);
-		} catch (ApiException e) {
-			e.printStackTrace();
-		}
-
-		// Add players to Notification
-		for (Player player: players.getPlayers()) {
-			if (player.getId().equals(thisDeviceId)) { // Jump over player's own device
-				continue;
-			}
-
-			notification.addIncludePlayerIdsItem(player.getId());
-		}
-
-		// Send Notification
-		{
-			try {
-				apiInstance.createNotification(notification);
-				System.out.println("OneSignal Invite Sent Successfully!");
-			} catch (ApiException e) {
-				System.err.println("Exception when calling DefaultApi#createNotification");
-				System.err.println("Status code: " + e.getCode());
-				System.err.println("Reason: " + e.getResponseBody());
-				System.err.println("Response headers: " + e.getResponseHeaders());
-				e.printStackTrace();
-			}
-		}
 	}
 }
